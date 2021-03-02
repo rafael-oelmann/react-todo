63cf4b55ef38c5e93069550faa951101dbcaeaa6
Rafael <rafael.oelmann@gmail.com>
2021-03-02 14:58:38 +0000

    moved prettierrc

---
+++ src/conponents/ToDoList.js
@@ -0,0 +1,23 @@
+import React from "react";
+import Todo from "./ToDo";
+
+const TodoList = ({ todos, setTodos, filteredTodos }) => {
+  console.log(todos);
+  return (
+    <div className="todo-container">
+      <ul className="todo-list">
+        {filteredTodos.map((todo) => (
+          <Todo
+            setTodos={setTodos}
+            todos={todos}
+            key={todo.id}
+            text={todo.text}
+            todo={todo}
+          />
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default TodoList;
